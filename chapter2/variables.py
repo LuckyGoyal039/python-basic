51a0709c8cc477f5f8850f277ed33a8f666c4554
Lucky Goyal <goyallucky2020@gmail.com>
2024-11-06 10:30:56 +0530

    play with variables

---
+++ chapter2/variables.py
@@ -0,0 +1,24 @@
+a=10
+b=20
+sum=a+b
+print(a+b)
+
+greater=a>b
+print(greater)
+
+equal=a==b
+print(equal)
+
+# a="hello"
+# concat="string" +34 #now allowed
+# concat=34+ "string" #now allowed
+
+c=31+31.33
+print(c)
+
+d=type(c)
+e=type("string")
+f=type(a)
+print(d)
+print(e)
+print(f)
